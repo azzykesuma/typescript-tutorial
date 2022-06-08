7a91929718f35d9272598b0ae4b7d192b139070e
azzy kesuma <official.azzy@gmail.com>
2022-06-08 08:29:43 +0700

    initial commit

---
+++ public/sandbox.js
@@ -0,0 +1,5 @@
+"use strict";
+let char;
+let numb;
+let mixedArr = [];
+mixedArr.push(10);
---
+++ src/sandbox.ts
@@ -0,0 +1,4 @@
+let char: string;
+let numb : Number;
+let mixedArr : (number | string)[] = [];
+mixedArr.push(10)
