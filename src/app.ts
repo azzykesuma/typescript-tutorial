107870c5d803b6bb9d8a013d20d2c0c0975e15d3
azzy kesuma <official.azzy@gmail.com>
2022-06-09 10:51:12 +0700

    finishing tuto

--- src/app.ts
+++ src/app.ts
@@ -46,8 +46,68 @@ import { listTemplate } from './classes/templateClass.js';
 //     console.log(inv.format());
 // })
 
+// generics
+// specifying the types of the variables, and getting all the props if it's an object
+// const addUID = <T extends object>(obj : T) => {
+//     let UID = Math.floor(Math.random() * 100);
+//     return {...obj, UID}
+// }
 
+// let docOne = addUID({name : 'azzy', age : 40});
+// console.log(docOne.age);
 
+// GENERICS WITH INTERFACES
+// interface is a blueprint for a class,
+// so anything that implements the interface must have the same exact
+// property of the interface
+// interface Resources<T> {
+//     uid : number;
+//     resourceName : string;
+//     // using the generics allow us to call any type of data
+//     data : T;
+// }
+// // if generics is implemented, then the interface must invoke 
+// // a specific data type that we want to use. 
+// const data:Resources<string[]> = {
+//     uid : 1,
+//     resourceName : 'michael',
+//     data : ['hello']
+// }
+
+// console.log(data);
+
+// ENUMS
+// enum is a way to create a list of named constants
+// then the list will be indexed, and we can call the index with the number associated with it.
+// enum ResourceType {BOOK,FILM,AUTHOR,DIRECTOR}
+// for example, if we want to use the book resource type, then we can call
+// ResourceType.BOOK, and if we log it, it will be 0, because book is at the first/[0],
+// of the index.
+// interface ResourceData<T> {
+//     UID : number;
+//     resourceName : ResourceType;
+//     data : T
+// }
+
+// const dataOne:ResourceData<object> = {
+//     UID : 1,
+//     resourceName : ResourceType.DIRECTOR,
+//     data : {skill : 'javascript'}
+// }
+
+// const dataTwo:ResourceData<object> = {
+//     UID : 2,
+//     resourceName : ResourceType.FILM,
+//     data : {skill : 'c#'}
+// }
+
+// console.log(dataOne,dataTwo);
+
+//  --------------TUPLES------------------
+// tuples are a way to create an array/object with specifics type of data in each position
+// const tup:[string,number,boolean] = ['michael',20,false];
+// const tupObj:{name : string, age : number} = {name : 'azzy', age : 30}
+// console.log(tupObj);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // grabbing input
@@ -63,11 +123,16 @@ const list = new listTemplate(ul);
 form.addEventListener('submit', (e:Event) => {
     e.preventDefault();
     let doc : hasFormatter;
+    let values: [string,string,number];
+    values = [toFrom.value,details.value,amount.valueAsNumber];
     
     if(type.value === 'invoice') {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        // we can't use the spred operator without using tuples
+        // because the spred will not know each of data type, whereas we have defined it
+        // specifically in the interface, hence the tuple. 
+        doc = new Invoice(...values);
     } else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     
     list.render(doc, type.value, 'start');
